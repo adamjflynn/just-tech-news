18323a19e72568bb56c2eb013ed0ee90f5b4c4ba
Adam Flynn <adamflynn1@gmail.com>
2022-01-02 15:02:08 -0600

    add seeds and template for single post

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const routes = require('./controllers');
 const path = require('path');
-const exphbs = require('express-handlebars');
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,16 +23,15 @@ app.use(session(sess));
 
 const hbs = exphbs.create({});
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(require('./controllers/'));
-
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
